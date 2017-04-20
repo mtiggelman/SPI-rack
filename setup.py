5360406aa692412fbf67028c6084c78e6b774871
Ruben van Gulik <rubenvangulik@gmail.com>
2017-04-20 13:36:57 +0200

    initial package structure

---
+++ setup.py
@@ -0,0 +1,18 @@
+from setuptools import setup
+
+setup(name='spirack',
+      version='0.1.0.dev1',
+      description='Drivers for the QuTech SPI-rack',
+      url='https://github.com/Rubenknex/SPI-rack',
+      author='Marijn Tiggelman',
+      author_email='marijn.tiggelman@gmail.com',
+      license='MIT',
+      packages=['spirack',
+                'spirack/drivers'
+                'tests'],
+      install_requires=[
+        'serial',
+      ],
+      package_data={
+        '': ['*.cat', '*.inf']
+      })
