b4ab811c0b2cbd1b8ae70ac28760328a34c8628f
mtiggelman <m.j.tiggelman@tudelft.nl>
2017-05-02 13:50:17 +0200

    Small fix in setup.py

--- setup.py
+++ setup.py
@@ -1,7 +1,7 @@
 from setuptools import setup
 
 setup(name='spirack',
-      version='0.1.0.dev1',
+      version='0.1.0.dev2',
       description='Drivers for the QuTech SPI-rack',
       url='https://github.com/Rubenknex/SPI-rack',
       download_url='https://github.com/Rubenknex/SPI-rack/archive/0.1.0.dev1.tar.gz',
@@ -12,7 +12,7 @@ setup(name='spirack',
       keyword = ['SPI', 'Qcodes'],
       classifiers = [],
       install_requires=[
-        'serial',
+        'pyserial',
       ],
       package_data={
         '': ['*.cat', '*.inf']
