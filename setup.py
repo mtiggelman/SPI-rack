2dac789002a53e1e729c23494b3d5b72b7dcce3b
Ruben van Gulik <rubenvangulik@gmail.com>
2017-05-03 13:33:35 +0200

    install driver in setup.py

--- setup.py
+++ setup.py
@@ -1,5 +1,16 @@
 from setuptools import setup
 
+import os
+import subprocess
+
+path = os.path.join(os.getcwd(), 'spirack', 'drivers', 'SPI_Rack_Controller.inf')
+
+cmd = 'C:\\Windows\\System32\\PNPUTIL.exe -i -a {}'
+proc = subprocess.Popen([cmd.format(path)], stdout=subprocess.PIPE, shell=True)
+out, err = proc.communicate()
+
+print(out)
+
 setup(name='spirack',
       version='0.1.0.dev2',
       description='Drivers for the QuTech SPI-rack',
@@ -8,7 +19,7 @@ setup(name='spirack',
       author='Marijn Tiggelman',
       author_email='qutechdev@gmail.com',
       license='MIT',
-      packages=['spirack'],
+      packages=['spirack', 'spirack/drivers'],
       keyword = ['SPI', 'Qcodes'],
       classifiers = [],
       install_requires=[
