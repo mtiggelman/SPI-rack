3665b3893aabb35a7585369b0d1f9f917acd27d1
Pieter Eendebak <pieter.eendebak@tno.nl>
2017-05-05 09:28:36 +0200

    add __version__ to package

--- setup.py
+++ setup.py
@@ -1,7 +1,27 @@
+import os
+import re
 from setuptools import setup
 
+def get_version(verbose=0):
+    """ Extract version information from source code """
+
+    try:
+        if os.path.exists('spirack/version.py'):
+            with open('spirack/version.py') as f:
+                ln = f.readline()
+                v = ln.split('=')
+                m = re.search('\'(.*)\'', ln)
+                FULLVERSION = (m.group(0)).strip().strip('\'').strip('"')
+        else:
+            FULLVERSION = '0.0'
+    except Exception as E:
+        FULLVERSION = '0.0'
+    if verbose:
+        print('get_version_info: %s' % FULLVERSION)
+    return FULLVERSION
+
 setup(name='spirack',
-      version='0.1.0.dev2',
+      version=get_version(),
       description='Drivers for the QuTech SPI-rack',
       url='https://github.com/Rubenknex/SPI-rack',
       download_url='https://github.com/Rubenknex/SPI-rack/archive/0.1.0.dev1.tar.gz',
@@ -9,7 +29,7 @@ setup(name='spirack',
       author_email='qutechdev@gmail.com',
       license='MIT',
       packages=['spirack'],
-      keyword = ['SPI', 'Qcodes'],
+      keywords = ['SPI', 'Qcodes'],
       classifiers = [],
       install_requires=[
         'pyserial',
